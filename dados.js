397e1e61f7a302a7dd47b510497de02d3e2ade0a
Joaquim-Renato <jocajoaquimas@gmail.com>
2024-09-04 20:23:44 -0300

    almanque 1.1

---
+++ dados.js
@@ -0,0 +1,17 @@
+let dados = [
+    {
+        titulo: "Anderson Herzer",
+        descricao: "Uma Voz Transformadora Anderson Herzer foi um poeta brasileiro e transexual que marcou a literatura nacional com sua obra mais conhecida, 'A Queda para o Alto'. Publicado em 1982, o livro é uma coletânea de poemas e textos autobiográficos que retratam, de forma visceral e poética, a experiência de uma pessoa trans vivendo em um mundo ainda pouco compreensivo e tolerante.",
+        link: "https://pt.wikipedia.org/wiki/Anderson_Herzer"
+    },
+    {
+        titulo: "Brenda Lee",
+        descricao: "Um Anjo da Guarda para as Travestis Brenda Lee foi uma figura emblemática da luta pelos direitos LGBTQIA+ no Brasil. Nascida em Bodocó, Pernambuco, em 1948, sua vida foi marcada por um incansável ativismo em prol da comunidade trans, especialmente das travestis.",
+        link: "https://pt.wikipedia.org/wiki/Brenda_Lee"
+    },
+    {
+        titulo: "João W. Nery",
+        descricao: "João W. Nery: Um Pioneiro na Luta pelos Direitos Trans no Brasil João W. Nery foi um psicólogo, escritor e ativista brasileiro que desempenhou um papel fundamental na luta pelos direitos da comunidade LGBTQIA+ no país. Nascido em 1950, no Rio de Janeiro, Nery se tornou uma figura histórica ao realizar, em 1977, a primeira cirurgia de redesignação sexual no Brasil, tornando-se assim o primeiro homem trans a passar por esse procedimento no país.",
+        link: "https://pt.wikipedia.org/wiki/Jo%C3%A3o_W._Nery"
+    }
+];
