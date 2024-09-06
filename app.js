165bd33859a79a0588af87a885e37cc5d4f46f4a
Joaquim-Renato <jocajoaquimas@gmail.com>
2024-09-06 20:28:52 -0300

    almanaque com js

--- app.js
+++ app.js
@@ -1,1 +1,45 @@
-console.log(dados);
+function pesquisar(){
+
+let section = document.getElementById("resultados-pesquisa");
+
+let campoPesquisa = document.getElementById("campoPesquisa").value
+
+if (!campoPesquisa){
+    section.innerHTML = "<p> Nada foi encontrado. Certifique-se de que escreveu o nome corretamente ;) "
+    return
+}
+
+    campoPesquisa = campoPesquisa.toLowerCase()
+
+    let resultados = "";
+    let titulo = "";
+    let descricao = "";
+    let tags = "";
+
+    for (let dado of dados) {
+        titulo = dado.titulo.toLowerCase()
+        descricao = dado.descricao.toLowerCase()
+        tags = dado.tags.toLowerCase()
+        // se titulo includes campoPesquisa
+        if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+            // cria um novo elemento
+            resultados += `
+            <div class="item-resultado">
+                <h2>
+                    <a href="#" target="_blank">${dado.titulo}</a>
+                </h2>
+                <p class="descricao-meta">${dado.descricao}</p>
+                <a href=${dado.link} target="_blank">Mais informações</a>
+            </div>
+        `;
+        }
+    }
+
+    if (!resultados) {
+        resultados = "<p>Nada foi encontrado</p>"
+    }
+
+    // Atribui os resultados gerados à seção HTML
+    section.innerHTML = resultados;
+}
+
